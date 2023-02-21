3e63652b76c6ce7f2a16e5d3dafeca8a9c493ee8
Trynce Brandt <tjbrandt2013@gmail.com>
2023-02-21 10:15:19 -0500

    setup login on controllers and routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -37,3 +37,18 @@ export const register = async (req, res) => {
         res.status(500).json({ error: err.message })
     }
 }
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email: email })
+        if (!user) return res.status(400).json({ msg: "User does not exist. "})
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credetials. "})
+
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
