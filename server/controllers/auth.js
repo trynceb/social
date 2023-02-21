a6f7d921893d7818c93b4e8fbcc57db9f2e885ae
Trynce Brandt <tjbrandt2013@gmail.com>
2023-02-21 10:17:44 -0500

    setup login on controllers and routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -48,6 +48,10 @@ export const login = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) return res.status(400).json({ msg: "Invalid credetials. "})
 
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        delete user.password
+        res.status(200).json({ token, user })
+        
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
