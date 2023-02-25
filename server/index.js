beec180a5abd12774513c41a406f5670ee0f9c62
Trynce Brandt <tjbrandt2013@gmail.com>
2023-02-25 08:25:43 -0500

    add authentication middleware

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path"
 import { fileURLToPath } from "url"
 import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js"
+import { verifyToken } from "./middleware/auth.js"
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url)
@@ -40,7 +41,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register)
+app.post("/auth/register", upload.single("picture"), verifyToken, register)
 
 /* ROUTES */
 app.use("/auth", authRoutes)
