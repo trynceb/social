85f0ac0d9d7234bf53da3117c502b04f615fca5d
Trynce Brandt <tjbrandt2013@gmail.com>
2023-02-25 13:28:46 -0500

    fix import issues

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { getUser, getUserFriends, addRemoveFriend } from "../controllers/users.js"
-import { verifyToken } from "../middleware/auth"
+import { verifyToken } from "../middleware/auth.js"
 
 const router = express.Router()
 
