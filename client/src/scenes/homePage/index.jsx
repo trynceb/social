4812c1ec0804d81d18cd7e623025dd83158a0e35
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-02 14:56:52 -0500

    add UserWidget to HomePage and fix bug in widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import NavBar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 
 const HomePage = () => {
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
+    const { _id, picturPath } = useSelector((state) => state.user)
+
     return (
         <Box>
             <NavBar />
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreens ? "flex" : "block"}
+                gap="0.5rem"
+                justifyContent="space-between"
+            >
+                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+                    <UserWidget userId={_id} picturePath={picturPath} />
+                </Box>
+                <Box 
+                    flexBasis={isNonMobileScreens ? "42%" : undefined}
+                    mt={isNonMobileScreens ? undefined : "2rem"}
+                >
+
+                </Box>
+                {isNonMobileScreens && (
+                    <Box flexBasis="26%">
+                    </Box>
+                )}
+            </Box>
         </Box>
     )
 }
