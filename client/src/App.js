e924b6ab24ce6632f4b74f28c8193c80af7ad115
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-02 13:00:51 -0500

    fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
-import HomePage from 'scenes/homePage';
-import LoginPage from 'scenes/loginPage';
-import ProfilePage from 'scenes/profilePage';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings } from 'theme';
+import { themeSettings } from "./theme";
 
 function App() {
-  const mode = useSelector((state) => state.mode)
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
-  const isAuth = Boolean(useSelector((state) => state.token))
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -20,8 +20,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
-            <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,36 +1,36 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material"
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import Form from "./Form";
 
 const LoginPage = () => {
-    const theme = useTheme()
-    const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="primary">
+          The Social
+        </Typography>
+      </Box>
 
-    return (
-        <Box>
-            <Box width="100%" backgroundColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
-                <Typography
-                    fontWeight="bold"
-                    fontSize="32px"
-                    color="primary"
-                >
-                    The Social
-                </Typography>
-            </Box>
-            <Box
-                width={isNonMobileScreens ? "50%" : "93%"}
-                p="2rem"
-                m="2rem auto"
-                borderRadius="1.5rem"
-                backgroundColor={theme.palette.background.alt}
-            >
-                <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
-                    Welcome to The Social!
-                </Typography>
-                <Form />
-            </Box>
-        </Box>
-            
-    )
-}
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to The Social!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
