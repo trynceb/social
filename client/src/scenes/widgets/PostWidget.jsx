c0aea73ae18d2de56d9324e48374ab8063e74b82
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-03 08:42:42 -0500

    create friend component

---
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -0,0 +1,3 @@
+import { ChatBubbleOutlineOutlined, FavoriteBorderOutlines, FavoriteBorderOutlined, ShareOutlined } from "@mui/icons-material";
+import { Box, Divider, IconButton, Typography, useTheme } from "@mui/system";
+import FlexBetween from "components/FlexBetween";
