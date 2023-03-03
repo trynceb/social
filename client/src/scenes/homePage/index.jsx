f2307329cd474169534dd0220f388b69e4066d0a
Trynce Brandt <tjbrandt2013@gmail.com>
2023-03-03 10:37:09 -0500

    fix import and spelling issues in PostsWidget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,42 +1,42 @@
 import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
-import NavBar from "scenes/navbar";
+import Navbar from "scenes/navbar";
 import UserWidget from "scenes/widgets/UserWidget";
-import MyPostWidget from "scenes/widgets/MyPostWidget"
+import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 
-
 const HomePage = () => {
-    const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
-    const { _id, picturePath } = useSelector((state) => state.user)
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
 
-    return (
-        <Box>
-            <NavBar />
-            <Box
-                width="100%"
-                padding="2rem 6%"
-                display={isNonMobileScreens ? "flex" : "block"}
-                gap="0.5rem"
-                justifyContent="space-between"
-            >
-                <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-                    <UserWidget userId={_id} picturePath={picturePath} />
-                </Box>
-                <Box 
-                    flexBasis={isNonMobileScreens ? "42%" : undefined}
-                    mt={isNonMobileScreens ? undefined : "2rem"}
-                >
-                    <MyPostWidget picturePath={picturePath} />
-                    <PostsWidget userId={_id} />
-                </Box>
-                {isNonMobileScreens && (
-                    <Box flexBasis="26%">
-                    </Box>
-                )}
-            </Box>
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
-    )
-}
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <Box m="2rem 0" />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
 
-export default HomePage
+export default HomePage;
